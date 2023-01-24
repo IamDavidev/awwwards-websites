b76056c88fa4a9632f53b4531f2ad86935a2c84a
itsDavidev <davidlez2mgk@gmail.com>
2023-01-24 13:25:31 -0600

    💫 animations for news sections

--- src/pages/caroselling-it/utils/animations.ts
+++ src/pages/caroselling-it/utils/animations.ts
@@ -16,10 +16,16 @@ const $servizi = $('.ani-servizi') as HTMLElement;
 
 const $sectionsDescription = $$('.container-des');
 
-const $wrapperImg = $('.ani-ind') as HTMLElement;
-const $imgParallax = $('.parallax-img') as HTMLElement;
+const $wrappersImg = $$('.ani-ind');
+const $imgsParallax = $$('.parallax-img');
 
 const $gridNumberItme = $$('.ani-grid-number');
+
+const $headersLetter = $$('.ani-header-lettere');
+console.info('🚀 ~>  file: animations.ts:25 ~>  art', $headersLetter);
+// const img = art.querySelector('img') as HTMLElement;
+
+// const img =;
 
 fadeIn($nabar);
 slideInWithFadeIn($headerTitle);
@@ -42,14 +48,27 @@ $gridNumberItme.forEach(item => {
 	});
 });
 
-scroll(
-	({ y }) => {
-		const yProgress = Number(y.progress.toFixed(2));
-		const valueTranslate = yProgress * -240;
-		$imgParallax.style.transform = `translateY(${valueTranslate}px)`;
-	},
-	{
-		offset: ['start end', '-50px -50px'],
-		target: $wrapperImg,
-	}
-);
+$imgsParallax.forEach((img: HTMLElement, index) => {
+	scroll(
+		({ y }) => {
+			const yProgress = Number(y.progress.toFixed(2));
+			const valueTranslate = yProgress * -240;
+			img.style.transform = `translateY(${valueTranslate}px)`;
+		},
+		{
+			offset: ['start end', '-50px -50px'],
+			target: $wrappersImg[index],
+		}
+	);
+});
+$headersLetter.forEach((art: HTMLElement) => {
+	const img = art.querySelector('img') as HTMLElement;
+	art.addEventListener('mouseenter', () => {
+		img.style.transform = 'translate3d(-1.5rem, -1.5rem, .5rem)';
+		img.style.transition = 'transform 0.5s ease-in-out';
+	});
+	art.addEventListener('mouseleave', () => {
+		img.style.transform = 'translate3d(0, 0, 0)';
+		img.style.transition = 'transform 0.5s ease-in-out';
+	});
+});
